e275f000af7413c92b7ef16e4c801c43c6bcdc35
rafaelzanirato <84473402+rafaelzanirato@users.noreply.github.com>
2021-05-19 16:20:20 -0300

    Add files via upload

---
+++ rafael_210519.py
@@ -0,0 +1,45 @@
+# Question 2
+name = ("Rafael Bote Candido Zanirato")
+# Question 3
+age = ("16")
+# Question 4
+print(name + " " + age)
+# Question 5
+print(name + age + "!")
+# Question 6
+quatro = (name+" "+age)
+print(quatro+"!")
+# Question 7
+nome= "Rafael"
+sobrenome= "Zanirato"
+rico=(nome+" "+sobrenome)
+# Question 8
+print(rico)
+# Question 9
+print(rico.upper())
+# Question 10
+print(rico.lower())
+# Question 11 
+print(rico.title())
+# Question 12
+print("rafael\nzanirato")
+# Question 13
+print("rafael\n\tzanirato")
+# Question 14
+food = " churrasco "
+# Question 15
+print(food.lstrip())
+# Question 16
+print(food.lstrip() + "!")
+# Question 17 
+print(food.strip() + "!")
+# Question 18
+print(102/23)
+# Question 19
+user_sol=input("What's your favourite food? ")
+print(user_sol.upper() + " " + user_sol.upper() + " " + user_sol.upper() + "!")
+# Question 20 
+print(1+1)
+print("1"+"1")
+print(str(1)+"1")
+print(1+int("1"))
